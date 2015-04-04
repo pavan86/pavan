fbb942869fea16b99333a17b3562bc1bcb7205b9
root <root@puppetmaster.(none)>
2015-04-05 00:31:47 +0530

    test new repo

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+$dir=__DIR__."/"."php_downs"."/"."img.jpg";
+$myfile = fopen($dir, "w");
+echo $dir;
+$source = "https://i.ytimg.com/vi/-S-LAFsEz2U/hqdefault.jpg";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $source);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSLVERSION,3);
+$data = curl_exec ($ch);
+//echo $data;
+$error = curl_error($ch); 
+$file = fopen($dir, "w");
+fputs($file, $data);
+curl_close ($ch);
+ 
+?>
